c5407e9a3422f45b34878ed65ed4dc96c0c180cc
Kevin <kana46@windowslive.com>
2023-02-13 00:07:47 +0200

    Update server.js

--- server.js
+++ server.js
@@ -115,7 +115,7 @@ function processRequest(req, res) {
         }
         
         // Remove origin and referer headers. TODO: This is a bit naughty, we should remove at some point.
-        // delete req.headers["origin"];
+        delete req.headers["origin"];
         delete req.headers["referer"];
 
         const proxyRequest = request({
